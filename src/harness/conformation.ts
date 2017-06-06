c6e2866dedf8df1d2b774db19d93819923e846e6
Evan Sebastian <evanlhoini@gmail.com>
2017-06-06 11:49:32 +0800

    Fix lint errors

--- src/harness/conformation.ts
+++ src/harness/conformation.ts
@@ -6,7 +6,6 @@ import { parse } from 'acorn'
 import { Map, Stack, List } from 'immutable'
 
 import { evalStatement, State, Scope } from '../../src/evaluator'
-import { blocking } from '../../src/scheduler'
 
 const fixturesFolderPath = path.resolve(__dirname, '..', '..', 'test', 'fixtures')
 
@@ -22,7 +21,6 @@ export type ConformationTest = {
 export const loadAndParseConformation = (fixtureFilePath: string): ConformationTest[] => {
   const pathname = path.join(fixturesFolderPath, fixtureFilePath)
   const content = fs.readFileSync(pathname, 'utf8')
-  const comments: string[] = []
   const expectedValues: any[] = []
   const tests: ConformationTest[] = []
 
