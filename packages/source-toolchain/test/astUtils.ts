a8cd04f5fac32a3d13ce2a3482c60407fd6f4d4f
Evan Sebastian <evanlhoini@gmail.com>
2017-06-08 20:53:30 +0800

    Forgot to test closure

--- packages/source-toolchain/test/astUtils.ts
+++ packages/source-toolchain/test/astUtils.ts
@@ -1,5 +1,6 @@
 import * as es from 'estree'
 import { createNode } from '../src/astUtils'
+import Closure from  '../src/Closure'
 
 it('createNode correctly creates Node from value', () => {
   const n1 = createNode(3) as es.Literal
@@ -13,4 +14,9 @@ it('createNode correctly creates Node from value', () => {
   const n3 = createNode('Hello') as es.Literal
   expect(n3.type).toBe('Literal')
   expect(n3.value).toBe('Hello')
+
+  const node: any = {}
+  const fn = new Closure(node, 0)
+  const n4 = createNode(fn) 
+  expect(n4).toBe(node)
 })
